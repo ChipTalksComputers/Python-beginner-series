6f618aed3d09cf6d96b84d4ca4ba298fbefc3e78
Chip <chiptalkscomputers@gmail.com>
2020-05-03 10:26:47 +0530

    Create proj1.py
    THE CODE FOR UNDERSTANDING VARIABLES AND DATATYPES WHICH WAS COVERED IN MY YOUTUBE VIDEO.

---
+++ proj1.py
@@ -0,0 +1,17 @@
+#VARIABLES AND DATATYPES
+
+
+#print number using variables
+
+num = 10
+print(num)
+
+#convert to string
+
+num = str(10)
+print(type(num))
+
+#convert to float
+
+num = float(10)
+print(type(num))
