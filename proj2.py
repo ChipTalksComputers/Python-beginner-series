1e49212e187a3b8b3bc726129b997327a22efc61
Chip <chiptalkscomputers@gmail.com>
2020-05-11 09:34:19 +0530

    Create proj2.py
    This project contains all the things that you need to understand basics of input and output functions in python.

---
+++ proj2.py
@@ -0,0 +1,34 @@
+greeting = 'Hello wassup'
+name = 'Chip'
+
+#OUTPUT FUNCTION PRINT()
+
+#Usual output
+print(greeting, 'my name is', name)
+
+#Using sep parameter(separate by comma)
+print(greeting, 'my name is', name, sep=',')
+
+#Using end parameter(end with a space)
+print(greeting, 'my name is', name, end=' ')
+print('Hi')
+
+#Formatted output
+print(f'{greeting}, my name is {Chip}')
+
+#format() function
+print('{}, my name is {}'.format(greeting, name))
+
+
+#INPUT FUNCTION INPUT()
+
+#Input with prompt
+greeting = input('Enter any greeting')
+name = input("What's your name?")
+
+print(greeting, name)
+
+#eval() function
+exp = '5+9'
+print(eval(exp))
+
