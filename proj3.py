5842e8ca5f973ffe868912409a410ad4cd7d5098
Chip <chiptalkscomputers@gmail.com>
2020-05-18 14:06:46 +0530

    Create proj3.py
    This contains a list of arithmetic and comparison operators in Python explained in my YouTube video.

---
+++ proj3.py
@@ -0,0 +1,64 @@
+#Arithmetic operators
+
+a = 5
+b = 6
+
+# + operator
+
+print(a+b)  #Adds a and b
+
+# - operator
+
+print(a-b)  #Subtracts b from a
+
+# * operator
+
+print(a*b)  #Adds a, b times 
+
+# / operator
+
+print(a/b)  #Quotient when a is divided by b
+
+# % operator
+
+print(a%b)  #Remainder when int(a) is divided by int(b)
+
+# // operator
+
+print(a//b) #Quotient when int(a) is divided by int(b)
+
+# ** operator
+
+print(a**b) #Returns a raised to b
+
+
+
+
+# Comparison operators
+
+# < operator
+
+print(a<b) #Returns True is a is less than b, else False
+
+# > operator
+
+print(a>b) #Returns True if a is greater than b, else False
+
+# <= operator
+
+print(a<=b) #Returns True if a is either less than or equal to b, else False
+
+# >= operator
+
+print(a>=b) #Returns True if a is either greater than or equal to b, else False
+
+# != operator
+
+print(a!=b) #Returns True if a is not equal to b, else False
+
+# == operator
+
+print(a==b) #Returns True if a is equal to b, else False
+
+
+
