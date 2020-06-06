111aa52819b6bc12cdbc2f1ec4e4a82904fd84e3
Chip <chiptalkscomputers@gmail.com>
2020-06-06 18:37:07 +0530

    Create proj6.py
    This file contains the code that is required for following along with the video that I made on my YouTube channel -
    https://youtu.be/iMsh3wuGjMc

---
+++ proj6.py
@@ -0,0 +1,25 @@
+rodents = ['squirrel', 'chipmunk', 'hamster']
+
+print(rodents)
+
+rodents.insert(0, 'rat')
+
+print(rodents)
+
+rodents.remove('chipmunk')
+
+print(rodents)
+
+rodents2 = rodents.copy()
+
+print(rodents2)
+
+rodents.clear()
+
+rodents.append('rat')
+
+print(rodents)
+
+rodents.pop(0)
+
+print(rodents)
