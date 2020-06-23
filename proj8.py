82ecaaf93dd04734788ec2eebab8f025d727e5e0
Chip <chiptalkscomputers@gmail.com>
2020-06-23 13:35:36 +0530

    Create proj8.py
    The code required to follow along with the Tuples tutorial can be found here! Here's the link to the video: https://youtu.be/tlGzBTL4wX8

---
+++ proj8.py
@@ -0,0 +1,27 @@
+#Tuples
+
+alphabets = ['a', 'b', 'c']
+
+print(alphabets)
+
+del alphabets
+
+print(alphabets)    #Error
+
+alphabets = 'a', 'b', 'c'
+
+print(alphabets)
+
+print(alphabets[2])
+
+print(alphabets[-1])
+
+print(alpphabets[1:3])
+
+print(alphabets.index('b'))
+
+print(alphabets.count('a'))
+
+
+
+
