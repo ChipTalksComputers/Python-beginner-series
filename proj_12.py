63507dea03c7354f1a3ebc206e5342cff7720f1b
Chip <chiptalkscomputers@gmail.com>
2020-07-19 16:39:33 +0530

    Create proj_12.py
    The code required for you to follow along with my video on Functions can be found here. Here's the link to the video: https://youtu.be/02rCxXE15ck

---
+++ proj_12.py
@@ -0,0 +1,23 @@
+#Functions
+
+def print_message():
+  print("Chip is a good squirrel")
+
+print_message()
+
+def print_message1(adjective = "good"):
+  print("Chip is a "+adjective+" squirrel")
+  
+print_message1("cute")
+print_message1()
+
+def print_message2(*adjective):
+  print("Chip is a "+adjective[2]+" squirrel")
+  
+print_message2("cute", "clever", "naughty")
+
+def print_message3(**adjective):
+  print("Chip is a "+adjective[adj2]+" squirrel")
+  
+print_message3(adj1 = "cute", adj2 = "clever", adj3 = "squirrel")
+
