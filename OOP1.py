3d20b3ec3ce778509215fd6557c0ffc1d0210d57
Chip <chiptalkscomputers@gmail.com>
2020-08-09 18:20:53 +0530

    Create OOP1.py
    The code required for you to follow along with the OOP-Part 1 tutorial on my YouTube channel can be found here. Here's the link to the video: https://youtu.be/u96StQ3xILc

---
+++ OOP1.py
@@ -0,0 +1,16 @@
+#OOP
+
+class houseBluePrint:
+  material_walls = 'birch'       #Change to oak for the second house
+  material_roof = 'jungle'       #Change to spruce for the second house
+  
+  def build_wall(self):
+    print(f'Build the walls using {self.material_walls}')
+  def build_roof(self):
+    print(f'Build the roof using {self.material_roof}')
+  
+  
+ birch_house = houseBluePrint()  #Change based on the house
+ birch_house.build_wall()         
+ birch_house.build_roof()
+    
