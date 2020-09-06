a2402309ad689b68e8db0154f3f68d04cb3796b9
Chip <chiptalkscomputers@gmail.com>
2020-09-06 20:40:44 +0530

    Update OOP2.py

--- OOP2.py
+++ OOP2.py
@@ -1,4 +1,5 @@
 #OOP
+#Important: The indentation space I've used here is 2. You'll need to make the changes, if any on your compiler!
 
 class primitive_houseBluePrint:
   def __init__(self, walls, roof):
