05c91afb53fb90908dc278140ccfca3dc3ca68ed
Chip <chiptalkscomputers@gmail.com>
2020-09-13 20:32:28 +0530

    Create math_module.py
    The code required for you to follow along with the math module tutorial on my channel can be found here. Here's the link to the YouTube video:

---
+++ math_module.py
@@ -0,0 +1,40 @@
+#Math module
+
+import math
+
+#power
+print(math.pow(5,6)))
+
+#ceil
+print(math.ceil(12.2))
+
+#floor
+print(math.floor(12.2))
+
+#radians
+print(math.radians(180))
+
+#tangent(applies to all other ratios as well)
+print(math.tan(0))
+
+#inverse tangent(applies to all other ratios as well)
+print(math.atan(0))
+
+#hyperbolic(applies to all other ratios as well)
+print(math.tanh(0))
+
+#pi
+print(math.pi)
+
+#e(Euler's constant)
+print(math.e)
+
+#log
+print(math.log(2))
+
+#log_10
+print(math.log10(2))
+
+#exponential
+print(math.exp(2))
+
